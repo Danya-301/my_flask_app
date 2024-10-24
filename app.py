87bf6025206237f03c08ac8463a985741fa98dbf
Danya-301 <danya.sierrrra@gmail.com>
2024-10-24 14:42:54 -0500

    google drive modelo

--- app.py
+++ app.py
@@ -1,50 +1,59 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect, url_for
 from keras.applications.imagenet_utils import preprocess_input
 from keras.models import load_model
 import numpy as np
 import cv2
 import os
-import requests  # Asegúrate de tener esta importación
+import requests
 
 # Inicializa la app Flask
 app = Flask(__name__)
 
-# Lista de nombres de aves
-names = ['Amazona Alinaranja', 'Amazona de San Vicente', 'Amazona Mercenaria', 'Amazona Real',
-         'Aratinga de Pinceles', 'Aratinga de Wagler', 'Aratinga Ojiblanca', 'Aratinga Orejigualda',
-         'Aratinga Pertinaz', 'Batará Barrado', 'Batará Crestibarrado', 'Batara Crestinegro',
-         'Batará Mayor', 'Batará Pizarroso Occidental', 'Batará Unicolor', 'Cacatua Ninfa', 
-         'Catita Frentirrufa', 'Cotorra Colinegra', 'Cotorra Pechiparda', 'Cotorrita Alipinta',
-         'Cotorrita de Anteojos', 'Guacamaya Roja', 'Guacamaya Verde', 'Guacamayo Aliverde',
-         'Guacamayo azuliamarillo', 'Guacamayo Severo', 'Hormiguerito Coicorita Norteño',
-         'Hormiguerito Coicorita Sureño', 'Hormiguerito Flanquialbo', 'Hormiguerito Leonado',
-         'Hormiguerito Plomizo', 'Hormiguero Azabache', 'Hormiguero Cantor', 'Hormiguero de Parker',
-         'Hormiguero Dorsicastaño', 'Hormiguero Guardarribera Oriental', 'Hormiguero Inmaculado',
-         'Hormiguero Sencillo', 'Hormiguero Ventriblanco', 'Lorito Amazonico', 'Lorito Cabecigualdo',
-         'Lorito de fuertes', 'Loro Alibronceado', 'Loro Cabeciazul', 'Loro Cachetes Amarillos',
-         'Loro Corona Azul', 'Loro Tumultuoso', 'Ojodefuego Occidental', 'Periquito Alas Amarillas',
-         'Periquito Australiano', 'Periquito Barrado', 'Tiluchí Colilargo', 'Tiluchí de Santander',
-         'Tiluchi Lomirrufo']
-
-# Ruta del modelo
+# Nombre del archivo del modelo
 MODEL_PATH = 'modelo/model_VGG16_v4.keras'
 
+# Lista de nombres de aves
+names = [
+    'Amazona Alinaranja', 'Amazona de San Vicente', 'Amazona Mercenaria', 'Amazona Real',
+    'Aratinga de Pinceles', 'Aratinga de Wagler', 'Aratinga Ojiblanca', 'Aratinga Orejigualda',
+    'Aratinga Pertinaz', 'Batará Barrado', 'Batará Crestibarrado', 'Batara Crestinegro',
+    'Batará Mayor', 'Batará Pizarroso Occidental', 'Batará Unicolor', 'Cacatua Ninfa', 
+    'Catita Frentirrufa', 'Cotorra Colinegra', 'Cotorra Pechiparda', 'Cotorrita Alipinta',
+    'Cotorrita de Anteojos', 'Guacamaya Roja', 'Guacamaya Verde', 'Guacamayo Aliverde',
+    'Guacamayo azuliamarillo', 'Guacamayo Severo', 'Hormiguerito Coicorita Norteño',
+    'Hormiguerito Coicorita Sureño', 'Hormiguerito Flanquialbo', 'Hormiguerito Leonado',
+    'Hormiguerito Plomizo', 'Hormiguero Azabache', 'Hormiguero Cantor', 'Hormiguero de Parker',
+    'Hormiguero Dorsicastaño', 'Hormiguero Guardarribera Oriental', 'Hormiguero Inmaculado',
+    'Hormiguero Sencillo', 'Hormiguero Ventriblanco', 'Lorito Amazonico', 'Lorito Cabecigualdo',
+    'Lorito de fuertes', 'Loro Alibronceado', 'Loro Cabeciazul', 'Loro Cachetes Amarillos',
+    'Loro Corona Azul', 'Loro Tumultuoso', 'Ojodefuego Occidental', 'Periquito Alas Amarillas',
+    'Periquito Australiano', 'Periquito Barrado', 'Tiluchí Colilargo', 'Tiluchí de Santander',
+    'Tiluchi Lomirrufo'
+]
+
+# Función para descargar el modelo desde Google Drive
 def download_model():
     """Descarga el modelo desde Google Drive si no existe localmente."""
     if not os.path.exists(MODEL_PATH):
         url = 'https://drive.google.com/uc?export=download&id=1WEZ60x_yPY-gPv8ugoq_qDLTSAD541zc'
         print("Descargando modelo...")
-        response = requests.get(url, stream=True)
-        with open(MODEL_PATH, 'wb') as f:
-            for chunk in response.iter_content(chunk_size=8192):
-                f.write(chunk)
-        print("Modelo descargado exitosamente.")
-
-# Descarga el modelo si es necesario
-download_model()
+        try:
+            response = requests.get(url, stream=True)
+            response.raise_for_status()  # Lanza un error si la respuesta no es 200
+            with open(MODEL_PATH, 'wb') as f:
+                for chunk in response.iter_content(chunk_size=8192):
+                    f.write(chunk)
+            print("Modelo descargado exitosamente.")
+        except requests.exceptions.RequestException as e:
+            print(f"Error al descargar el modelo: {e}")
+            return False
+    return True
 
 # Carga el modelo
-model = load_model(MODEL_PATH)
+if download_model():
+    model = load_model(MODEL_PATH)
+else:
+    raise Exception("No se pudo descargar el modelo.")
 
 # Ruta de subida de imágenes
 UPLOAD_FOLDER = 'static'
